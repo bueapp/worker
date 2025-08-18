0a01cd3fb3d970b28632a3cc5cfd050e5896b89e
ryzm <114670723+ryzmae@users.noreply.github.com>
2025-08-18 21:09:19 +0200

    feat: Add ttl indexer

---
+++ src/ttl_index.rs
@@ -0,0 +1,32 @@
+use bson::doc;
+use mongodb::IndexModel;
+use mongodb::options::IndexOptions;
+
+use crate::models::{EventLog, GuardianLog};
+
+pub async fn ensure_ttl_indexes(
+    events_collection: &mongodb::Collection<EventLog>,
+    guardian_collection: &mongodb::Collection<GuardianLog>,
+) -> mongodb::error::Result<()> {
+    let events_index = IndexModel::builder()
+        .keys(doc! { "loggedAt": 1 })
+        .options(
+            IndexOptions::builder()
+                .expire_after(Some(std::time::Duration::from_secs(60 * 60 * 24 * 14)))
+                .build(),
+        )
+        .build();
+    events_collection.create_index(events_index).await?;
+
+    let guardian_index = IndexModel::builder()
+        .keys(doc! { "createdAt": 1 })
+        .options(
+            IndexOptions::builder()
+                .expire_after(Some(std::time::Duration::from_secs(60 * 60 * 24 * 14)))
+                .build(),
+        )
+        .build();
+    guardian_collection.create_index(guardian_index).await?;
+
+    Ok(())
+}
